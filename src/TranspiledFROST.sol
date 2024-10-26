ddaad6aff668b3383979f3fe60823bdfacea87f7
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-10-26 13:48:46 +0300

    feat: add transpiled version of FROST

---
+++ src/TranspiledFROST.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/**
+ * @dev Transpiled library for verifying `FROST-secp256k1-KECCAK256` signatures.
+ */
+library TranspiledFROST {
+    /**
+     * @dev Checks if public key `(x, y)` is on curve and that `x < Secp256k1.N`.
+     *      It also checks that `x % Secp256k1.N != 0`.
+     * @param publicKeyX Public key x.
+     * @param publicKeyY Public key y.
+     * @return _isValidPublicKey `true` if public key is valid, `false` otherwise.
+     */
+    function isValidPublicKey(uint256 publicKeyX, uint256 publicKeyY) internal pure returns (bool _isValidPublicKey) {
+        assembly ("memory-safe") {
+            function fun_isValidPublicKey(var_publicKeyX, var_publicKeyY) -> var {
+                let expr := lt(var_publicKeyX, not(0x014551231950b75fc4402da1732fc9bebe))
+                if expr {
+                    expr :=
+                        eq(
+                            mulmod(var_publicKeyY, var_publicKeyY, not(0x01000003d0)),
+                            addmod(
+                                mulmod(
+                                    var_publicKeyX,
+                                    mulmod(var_publicKeyX, var_publicKeyX, not(0x01000003d0)),
+                                    not(0x01000003d0)
+                                ),
+                                0x07,
+                                not(0x01000003d0)
+                            )
+                        )
+                }
+                var := expr
+            }
+            _isValidPublicKey := fun_isValidPublicKey(publicKeyX, publicKeyY)
+        }
+    }
+
+    /**
+     * @dev Verifies `FROST-secp256k1-KECCAK256` signature by formula $zG - cX = R$.
+     *      - Public key ($X$) must be checked with `FROST.isValidPublicKey(publicKeyX, publicKeyY)`.
+     *      - Signature R ($R$) must be on curve.
+     *      - Signature Z ($z$).
+     *      - Challenge ($c$) is computed via `FROST.computateChallenge(...)`.
+     * @param publicKeyX Public key x.
+     * @param publicKeyY Public key y.
+     * @param signatureRX Signature R x.
+     * @param signatureRY Signature R y.
+     * @param signatureZ Signature Z.
+     * @param messageHash Message hash.
+     * @return isValidSignature `true` if signature is valid, `false` otherwise.
+     */
+    function verifySignature(
+        uint256 publicKeyX,
+        uint256 publicKeyY,
+        uint256 signatureRX,
+        uint256 signatureRY,
+        uint256 signatureZ,
+        bytes32 messageHash
+    ) internal view returns (bool isValidSignature) {
+        assembly ("memory-safe") {
+            function fun_verifySignature(
+                var_publicKeyX, var_publicKeyY, var_signatureRX, var_signatureRY, var_signatureZ, var_messageHash
+            ) -> var {
+                var := 0
+                if iszero(
+                    eq(
+                        mulmod(var_signatureRY, var_signatureRY, not(0x01000003d0)),
+                        addmod(
+                            mulmod(
+                                var_signatureRX,
+                                mulmod(var_signatureRX, var_signatureRX, not(0x01000003d0)),
+                                not(0x01000003d0)
+                            ),
+                            0x07,
+                            not(0x01000003d0)
+                        )
+                    )
+                ) {
+                    var := 0
+                    leave
+                }
+                let var_1 := 0
+                let _1 := iszero(mod(var_signatureZ, not(0x014551231950b75fc4402da1732fc9bebe)))
+                var_1 := iszero(_1)
+                if _1 {
+                    var := 0
+                    leave
+                }
+                let expr := and(var_publicKeyY, 0x01)
+                let ret := 0
+                ret := 270
+                let var_memPtr := mload(0x40)
+                let usr$newFreePtr := add(var_memPtr, 288)
+                if or(gt(usr$newFreePtr, 0xffffffffffffffff), lt(usr$newFreePtr, var_memPtr)) { revert(0, 0) }
+                mstore(0x40, usr$newFreePtr)
+                calldatacopy(var_memPtr, calldatasize(), 0x88)
+                mstore8(add(var_memPtr, 0x88), add(and(var_signatureRY, 0x01), 0x02))
+                mstore(add(var_memPtr, 137), var_signatureRX)
+                mstore8(add(var_memPtr, 169), add(expr, 0x02))
+                mstore(add(var_memPtr, 170), var_publicKeyX)
+                mstore(add(var_memPtr, 202), var_messageHash)
+                mstore(add(var_memPtr, 234), 0x300046524f53542d736563703235366b312d4b454343414b3235362d763163)
+                mstore(add(var_memPtr, 266), shl(229, 0x03430b61))
+                let var_value := keccak256(var_memPtr, ret)
+                let _2 := add(var_memPtr, 204)
+                mstore(_2, var_value)
+                let _3 := add(var_memPtr, 236)
+                mstore8(_3, 0x01)
+                let ret_1 := 0
+                ret_1 := 66
+                let var_value_1 := keccak256(_2, 66)
+                mstore(_2, xor(var_value, var_value_1))
+                mstore8(_3, 0x02)
+                let expr_1 :=
+                    addmod(
+                        mulmod(shr(0x40, var_value_1), shl(192, 1), not(0x014551231950b75fc4402da1732fc9bebe)),
+                        or(and(shl(0x80, var_value_1), shl(128, 0xffffffffffffffff)), shr(0x80, keccak256(_2, 66))),
+                        not(0x014551231950b75fc4402da1732fc9bebe)
+                    )
+                let var_2 := 0
+                let _4 := iszero(mod(expr_1, not(0x014551231950b75fc4402da1732fc9bebe)))
+                var_2 := iszero(_4)
+                if _4 {
+                    var := 0
+                    leave
+                }
+                mstore(
+                    var_memPtr,
+                    sub(
+                        not(0x014551231950b75fc4402da1732fc9bebe),
+                        mulmod(var_signatureZ, var_publicKeyX, not(0x014551231950b75fc4402da1732fc9bebe))
+                    )
+                )
+                mstore(add(var_memPtr, 0x20), add(expr, 0x1b))
+                mstore(add(var_memPtr, 0x40), var_publicKeyX)
+                mstore(
+                    add(var_memPtr, 0x60),
+                    sub(
+                        not(0x014551231950b75fc4402da1732fc9bebe),
+                        mulmod(expr_1, var_publicKeyX, not(0x014551231950b75fc4402da1732fc9bebe))
+                    )
+                )
+                mstore(0, 0)
+                if iszero(staticcall(gas(), 0x01, var_memPtr, 0x80, 0, 0x20)) { revert(0, 0) }
+                let var_recovered := mload(0)
+                mstore(0, var_signatureRX)
+                mstore(0x20, var_signatureRY)
+                var := eq(var_recovered, and(keccak256(0, 0x40), sub(shl(160, 1), 1)))
+            }
+            isValidSignature :=
+                fun_verifySignature(publicKeyX, publicKeyY, signatureRX, signatureRY, signatureZ, messageHash)
+        }
+    }
+}
