94f130770004b90f29c5d8cc924306c7c153e61f
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-02-26 13:17:09 +0300

    feat: add module for modular exponentiation

---
+++ src/utils/cryptography/ModExp.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Memory} from "../Memory.sol";
+
+/**
+ * @dev Library for low-level modular exponentiation.
+ */
+library ModExp {
+    /**
+     * @dev Calculates modular exponentiation `(base ** exponent) % modulus`.
+     *      The fifth contract performs arbitrary-precision exponentiation under modulo.
+     *      Here, $0^0$ is taken to be one, and $x \mod 0$ is zero for all $x$
+     *      (from Ethereum Yellow Paper).
+     * @param memPtr Memory pointer for writing 192 bytes of input data.
+     * @param base Base.
+     * @param exponent Exponent.
+     * @param modulus Modulus.
+     * @return result Modular exponentiation result.
+     */
+    function modexp(uint256 memPtr, uint256 base, uint256 exponent, uint256 modulus)
+        internal
+        view
+        returns (uint256 result)
+    {
+        Memory.writeWord(memPtr, 0x00, 0x20);
+        Memory.writeWord(memPtr, 0x20, 0x20);
+        Memory.writeWord(memPtr, 0x40, 0x20);
+        Memory.writeWord(memPtr, 0x60, base);
+        Memory.writeWord(memPtr, 0x80, exponent);
+        Memory.writeWord(memPtr, 0xa0, modulus);
+
+        Memory.writeWord(0x00, 0x00, 0x00);
+
+        // https://evm.codes/precompiled#0x05
+        assembly ("memory-safe") {
+            pop(staticcall(gas(), 0x05, memPtr, 0xc0, 0x00, 0x20))
+            result := mload(0x00)
+        }
+    }
+}
---
+++ test/utils/cryptography/ModExp.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ModExp} from "src/utils/cryptography/ModExp.sol";
+import {Memory} from "src/utils/Memory.sol";
+
+contract ModExpTest is Test {
+    function test_ModExpSimpleMod1024() public view {
+        uint256 memPtr = Memory.allocate(192);
+        uint256 base = 2;
+        uint256 exponent = 9;
+        uint256 modulus = 1024;
+        assertEq(ModExp.modexp(memPtr, base, exponent, modulus), 512);
+    }
+
+    function test_ModExpSimpleMod10() public view {
+        uint256 memPtr = Memory.allocate(192);
+        uint256 base = 2;
+        uint256 exponent = 9;
+        uint256 modulus = 10;
+        assertEq(ModExp.modexp(memPtr, base, exponent, modulus), 2);
+    }
+
+    function test_ModExpZeroToThePowerOfZero() public view {
+        uint256 memPtr = Memory.allocate(192);
+        uint256 base = 0;
+        uint256 exponent = 0;
+        uint256 modulus = 42;
+        assertEq(ModExp.modexp(memPtr, base, exponent, modulus), 1);
+    }
+
+    function test_ModExpMod0() public view {
+        uint256 memPtr = Memory.allocate(192);
+        uint256 base = 2;
+        uint256 exponent = 9;
+        uint256 modulus = 0;
+        assertEq(ModExp.modexp(memPtr, base, exponent, modulus), 0);
+    }
+}
