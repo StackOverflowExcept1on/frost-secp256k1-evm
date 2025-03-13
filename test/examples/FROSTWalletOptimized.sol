009f1f02d39759a7c30ed1abe1821caa2f6629f9
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-03-13 12:34:40 +0300

    feat: add `FROSTWalletOptimized` to `test/examples`

---
+++ test/examples/FROSTWalletOptimized.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Hashes} from "src/utils/cryptography/Hashes.sol";
+import {Memory} from "src/utils/Memory.sol";
+import {FROST} from "src/FROST.sol";
+
+contract FROSTWalletOptimized {
+    uint256 nonce;
+
+    // cargo run --release --manifest-path offchain-signer/Cargo.toml
+    uint256 internal constant PUBLIC_KEY_X = 0x4F6340CFDD930A6F54E730188E3071D150877FA664945FB6F120C18B56CE1C09;
+    uint256 internal constant PUBLIC_KEY_Y = 0x802A5E67C00A70D85B9A088EAC7CF5B9FB46AC5C0B2BD7D1E189FAC210F6B7EF;
+
+    constructor() payable {
+        require(FROST.isValidPublicKey(PUBLIC_KEY_X, PUBLIC_KEY_Y));
+    }
+
+    function executeTransaction5112088248(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        uint256 signatureRX,
+        uint256 signatureRY,
+        uint256 signatureZ
+    ) external payable {
+        uint256 size;
+        unchecked {
+            size = 0xc0 + data.length;
+        }
+        uint256 memPtr = Memory.allocate(size);
+
+        Memory.copyFromCalldata(memPtr, 0xc0, data);
+        uint256 dataHash = Hashes.efficientKeccak256(memPtr, 0xc0, data.length);
+
+        Memory.writeWord(memPtr, 0x00, block.chainid);
+        Memory.writeWord(memPtr, 0x20, uint256(uint160(address(this))));
+        Memory.writeWord(memPtr, 0x40, nonce);
+        unchecked {
+            nonce++;
+        }
+        Memory.writeWord(memPtr, 0x60, uint256(uint160(to)));
+        Memory.writeWord(memPtr, 0x80, value);
+        Memory.writeWord(memPtr, 0xa0, dataHash);
+
+        bytes32 messageHash = bytes32(Hashes.efficientKeccak256(memPtr, 0x00, 0xc0));
+
+        // NOTE: `require(FROST.isValidPublicKey(...))` is checked in constructor
+        require(FROST.verifySignature(PUBLIC_KEY_X, PUBLIC_KEY_Y, signatureRX, signatureRY, signatureZ, messageHash));
+
+        bool success;
+        assembly ("memory-safe") {
+            success := call(gas(), to, value, add(memPtr, 0xc0), data.length, 0x00, 0x00)
+        }
+        require(success);
+    }
+
+    receive() external payable {}
+}
---
+++ test/examples/FROSTWalletOptimized.t.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, Vm, console} from "forge-std/Test.sol";
+import {SigningKey, FROSTOffchain} from "src/FROSTOffchain.sol";
+import {FROSTWalletOptimized} from "./FROSTWalletOptimized.sol";
+
+library FROSTWalletOptimizedTestHelper {
+    Vm internal constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    using FROSTWalletOptimizedTestHelper for FROSTWalletOptimized;
+
+    function getNonce(FROSTWalletOptimized frostWallet) internal view returns (uint256) {
+        return uint256(vm.load(address(frostWallet), bytes32(uint256(0))));
+    }
+
+    function getMessageHash(FROSTWalletOptimized frostWallet, address to, uint256 value, bytes memory data)
+        public
+        view
+        returns (bytes32)
+    {
+        uint256 nonce = frostWallet.getNonce();
+        return getMessageHashWithNonce(frostWallet, to, value, data, nonce);
+    }
+
+    function getMessageHashWithNonce(
+        FROSTWalletOptimized frostWallet,
+        address to,
+        uint256 value,
+        bytes memory data,
+        uint256 nonce
+    ) public view returns (bytes32) {
+        bytes32 dataHash = keccak256(data);
+        return keccak256(
+            abi.encodePacked(
+                block.chainid, uint256(uint160(address(frostWallet))), nonce, uint256(uint160(to)), value, dataHash
+            )
+        );
+    }
+
+    function getTranscationData() public pure returns (address to, uint256 value, bytes memory data) {
+        return (0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045, 1 ether, "");
+    }
+}
+
+contract ReentrancyAttackToFROSTWalletOptimized {
+    using FROSTOffchain for SigningKey;
+    using FROSTWalletOptimizedTestHelper for FROSTWalletOptimized;
+
+    SigningKey signingKey;
+    FROSTWalletOptimized frostWallet;
+
+    uint256 nonce;
+
+    constructor(SigningKey _signingKey, FROSTWalletOptimized _frostWallet) {
+        signingKey = _signingKey;
+        frostWallet = _frostWallet;
+
+        nonce = frostWallet.getNonce();
+    }
+
+    function executeTransaction5112088248() public {
+        (address to, uint256 value, bytes memory data) = (address(this), 1 ether, "");
+        bytes32 messageHash = frostWallet.getMessageHashWithNonce(to, value, data, nonce);
+        (uint256 signatureRX, uint256 signatureRY, uint256 signatureZ) = signingKey.createSignature(messageHash);
+        frostWallet.executeTransaction5112088248(to, value, data, signatureRX, signatureRY, signatureZ);
+    }
+
+    receive() external payable {
+        if (address(frostWallet).balance > 0) {
+            executeTransaction5112088248();
+        }
+    }
+}
+
+contract FROSTWalletOptimizedTest is Test {
+    using FROSTOffchain for SigningKey;
+    using FROSTWalletOptimizedTestHelper for FROSTWalletOptimized;
+
+    SigningKey signingKey;
+    FROSTWalletOptimized frostWallet;
+
+    function setUp() public {
+        uint256 scalar = 0xA4DDF31F7F32BA696F14CE50ECF3F21E3E100E83BDF47966E7B07468E9500B6E;
+        signingKey = FROSTOffchain.signingKeyFromScalar(scalar);
+
+        Vm.Wallet memory wallet = vm.createWallet(signingKey.asScalar());
+
+        uint256 publicKeyX = wallet.publicKeyX;
+        uint256 publicKeyY = wallet.publicKeyY;
+
+        assertEq(publicKeyX, 0x4F6340CFDD930A6F54E730188E3071D150877FA664945FB6F120C18B56CE1C09);
+        assertEq(publicKeyY, 0x802A5E67C00A70D85B9A088EAC7CF5B9FB46AC5C0B2BD7D1E189FAC210F6B7EF);
+
+        frostWallet = new FROSTWalletOptimized{value: 100 ether}();
+        assertEq(address(frostWallet), DEFAULT_TEST_CONTRACT);
+    }
+
+    function test_ExecuteTransaction() public {
+        (address to, uint256 value, bytes memory data) = FROSTWalletOptimizedTestHelper.getTranscationData();
+        bytes32 messageHash = frostWallet.getMessageHash(to, value, data);
+        (uint256 signatureRX, uint256 signatureRY, uint256 signatureZ) = signingKey.createSignature(messageHash);
+        frostWallet.executeTransaction5112088248(to, value, data, signatureRX, signatureRY, signatureZ);
+    }
+
+    function test_ExecuteTransactionWithSignatureReplayAttack() public {
+        (address to, uint256 value, bytes memory data) = FROSTWalletOptimizedTestHelper.getTranscationData();
+        bytes32 messageHash = frostWallet.getMessageHash(to, value, data);
+        (uint256 signatureRX, uint256 signatureRY, uint256 signatureZ) = signingKey.createSignature(messageHash);
+        frostWallet.executeTransaction5112088248(to, value, data, signatureRX, signatureRY, signatureZ);
+
+        vm.expectRevert();
+        frostWallet.executeTransaction5112088248(to, value, data, signatureRX, signatureRY, signatureZ);
+    }
+
+    function test_ExecuteTransactionWithReentrancyAttack() public {
+        ReentrancyAttackToFROSTWalletOptimized proxy =
+            new ReentrancyAttackToFROSTWalletOptimized(signingKey, frostWallet);
+        try proxy.executeTransaction5112088248() {} catch {}
+        assertEq(address(proxy).balance, 0 ether);
+    }
+}
