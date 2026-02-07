e2c3fe176f60bd8f8b495eed6faff5a1764cc525
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2026-02-07 05:29:34 +0300

    chore(Hashes): add more functions

--- src/utils/cryptography/Hashes.sol
+++ src/utils/cryptography/Hashes.sol
@@ -20,11 +20,44 @@ library Hashes {
     }
 
     /**
+     * @dev Hashes memory chunk using `keccak256` instruction.
+     * @param memPtr Pointer to memory.
+     * @param offset Offset in memory.
+     * @param size Size of memory chunk to hash.
+     * @return value Hash of memory chunk.
+     */
+    function efficientKeccak256AsBytes32(uint256 memPtr, uint256 offset, uint256 size)
+        internal
+        pure
+        returns (bytes32 value)
+    {
+        assembly ("memory-safe") {
+            // https://evm.codes/#20
+            value := keccak256(add(memPtr, offset), size)
+        }
+    }
+
+    /**
      * @dev Implementation of `keccak256(abi.encode(a))` that doesn't allocate or expand memory.
      * @param a Value to hash.
      * @return value Hash of value.
      */
     function efficientKeccak256(uint256 a) internal pure returns (uint256 value) {
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.5.0/contracts/utils/cryptography/Hashes.sol
+        assembly ("memory-safe") {
+            // https://evm.codes/#52
+            mstore(0x00, a)
+            // https://evm.codes/#20
+            value := keccak256(0x00, 0x20)
+        }
+    }
+
+    /**
+     * @dev Implementation of `keccak256(abi.encode(a))` that doesn't allocate or expand memory.
+     * @param a Value to hash.
+     * @return value Hash of value.
+     */
+    function efficientKeccak256AsBytes32(bytes32 a) internal pure returns (bytes32 value) {
         // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.5.0/contracts/utils/cryptography/Hashes.sol
         assembly ("memory-safe") {
             // https://evm.codes/#52
@@ -41,6 +74,23 @@ library Hashes {
      * @return value Hash of first and second values.
      */
     function efficientKeccak256(uint256 a, uint256 b) internal pure returns (uint256 value) {
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.5.0/contracts/utils/cryptography/Hashes.sol
+        assembly ("memory-safe") {
+            // https://evm.codes/#52
+            mstore(0x00, a)
+            mstore(0x20, b)
+            // https://evm.codes/#20
+            value := keccak256(0x00, 0x40)
+        }
+    }
+
+    /**
+     * @dev Implementation of `keccak256(abi.encode(a, b))` that doesn't allocate or expand memory.
+     * @param a First value to hash.
+     * @param b Second value to hash.
+     * @return value Hash of first and second values.
+     */
+    function efficientKeccak256AsBytes32(bytes32 a, bytes32 b) internal pure returns (bytes32 value) {
         // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.5.0/contracts/utils/cryptography/Hashes.sol
         assembly ("memory-safe") {
             // https://evm.codes/#52
